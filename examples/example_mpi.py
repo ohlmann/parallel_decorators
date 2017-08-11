e54061a9318f2c04f6f4c0dd786316a3db6d97c2
Sebastian Ohlmann <sebastian.ohlmann@gmail.com>
2017-08-11 15:53:08 +0200

    improve MPI example

--- examples/example_mpi.py
+++ examples/example_mpi.py
@@ -4,14 +4,26 @@ from time import sleep
 import random
 
 
-@vectorize_parallel(method='MPI', use_progressbar=True, label='computation')
+@vectorize_parallel(method='MPI', use_progressbar=True, label='computation',
+                    scheduling='auto')
 def foo(i):
     sleep(0.1+random.random()*0.5)
     return i**2
 
 
-@vectorize_parallel(method='MPI', use_progressbar=True, label='computation')
+# with static scheduling, all numbers that are a multiple of 5 will be
+# computed only starting after 10 seconds
+@vectorize_parallel(method='MPI', use_progressbar=False, scheduling='static')
 def bar(i):
+    sleep(0.1+random.random()*0.5)
+    if (i == 5):
+        sleep(10)
+    print(i)
+    return i**2
+
+
+@vectorize_parallel(method='MPI', use_progressbar=True, label='computation')
+def baz(i):
     if i == 10000:
         return 1/0
     else:
@@ -23,4 +35,6 @@ result = foo(range(1, 100))
 if is_master():
     print(result[-4:])
 
-result2 = bar(range(1, 20000))
+result2 = bar(range(1, 100))
+
+result2 = baz(range(1, 20000))
